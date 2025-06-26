5348071e191a04ec7531df584a60649d344ade64
Tsion Teklay <tsionteklay18@gmail.com>
2025-06-26 04:14:39 +0300

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $host = "localhost";
 $db = "office_system";
 $user = "root";     // change if needed
-$pass = "";         // change if needed
+$pass = "";        // change if needed
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
